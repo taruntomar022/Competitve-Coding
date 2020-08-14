28c59ec9201fe0533da285afb7604338b60e3663
Tarun Tomar <taruntomar022@icloud.com>
2020-08-14 12:31:28 +0530

    Update leetcode_power_of_two.java

--- leetcode_power_of_two.java
+++ leetcode_power_of_two.java
@@ -1,15 +1,18 @@
 class Solution {
      public static  boolean isPowerOfTwo(int n) {
-         int count = 0;
-         while(n>0){
-              n = n&(n-1);
-             count++;
-         }
+//          int count = 0;
+//          while(n>0){
+//               n = n&(n-1);
+//              count++;
+//          }
         
-        if(count == 1){
-            return true;
-        }else{
-            return false;
-        }
+//         if(count == 1){
+//             return true;
+//         }else{
+//             return false;
+//         }
+          // complexity O(1)
+          Long N = (long)n;
+         return N>0 && (N&(N-1))==0; 
     }
 }
