630057426ba38e684be5925590f35d23a5aa91f4
Tarun Tomar <taruntomar022@icloud.com>
2020-08-14 13:32:52 +0530

    Single number 2 -> leetcode

---
+++ single_number2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int i=0;i<32;i++){
+            int count = 0;
+            int mask = (1<<i);
+            for(int ele : nums){
+                if((ele & mask) != 0) count++;
+            }
+            if(count % 3 != 0) ans |= mask;
+        }
+        return ans;
+    }
+}
